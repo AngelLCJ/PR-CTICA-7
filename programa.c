5adde6ad8a1bc4effb914d9607f06cd39b80d2b0
AngelLCJ <54157013+AngelLCJ@users.noreply.github.com>
2019-10-09 23:49:07 -0500

    Integrantes de la práctica 7.
    Correa Juarez Angel Leonardo
    Rojas Álvarez Robin Agustín
    Sicilia Andrade Pablo
    Valdés Zavala Enzo

---
+++ programa.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlin.h>
+
+int main(){
+
+float
+
+      short_; //Variable de tipo short      
+      signed int __; // Variable de entero
+      int v_1;Variable de tipo entero sin signo
+      unsigned int v_2; //Variable de tipo entero sin signo
+      lonng v_3; //Variable de tipo long
+      
+
+printf("Ingresa un dato tipo short\n");
+scanf("%hd",&_);
+printf("Ingresa un dato tipo signed int\n");
+scanf("%d",&__);
+printf("Ingresa un dato tipo int\n");
+scanf("%d",&v_1);
+printf("Ingresa un dato tipo unsigned int\n");
+scanf("%d",&__);
+printf("Ingresa un dato tipo long\n");
+scanf("%ld",&v_3);
+printf("Valores leídos\n");
+printf("%hd\t%d\t%d\t%d\t%ld\n",_,__,v_1,v_2,v_3);
+
+return 0;
+}
---
+++ programa2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlin.h>
+
+int main()
+{
+	char v1; //Variable de tipo char 
+	bool v2; //Variable de tipo bool
+	char v3[10]; //Cadena de 10 caracteres
+	printf("Ingresa un dato tipo char\n");
+	scanf("%c",&v1);
+	printf("Ingresa un dato tipo bool\n");
+	scanf("%d",&v2);
+	printf("Ingresa un dato tipo cadena\n");
+	scanf("%s",&v3)
+printf("Valores leídos\n");
+printf("%c\n",v1);
+printf("%d\n",v2);
+printf("%s\n",v3);
+return 0;
+}
